1fe27b0f11470d5d86b6f8f527efda7bd48e8892
https://github.com/Olgaroeva24/California-Real-Estate <olgaroeva24@gmail.com>
2021-07-15 17:10:57 +0300

    ati-SU1

--- ati-SU.py
+++ ati-SU.py
@@ -1,9 +1,8 @@
-from selenium import webdriver
-from selenium.common.exceptions import TimeoutException, NoSuchElementException
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC, wait
+import time
 
-driver = webdriver.Firefox()
+from selenium import webdriver
+
+driver = webdriver.Chrome()
 driver.get("https://ati.su/")
 driver.maximize_window()
 driver.implicitly_wait(5)
@@ -13,24 +12,17 @@ driver.find_element_by_xpath('//input[@id="input-geo-from"]').click()
 driver.find_element_by_xpath('//span[contains(text(),"Беларусь")]').click()
 driver.find_element_by_xpath('//input[@id="input-geo-to"]').click()
 driver.find_element_by_xpath("//span[contains(text(),'Россия')]").click()
+driver.execute_script("window.scrollTo(0, 400)")
 driver.find_element_by_xpath('//*[@class="_2c8wr-2-0-680  _38IFK-2-0-680 _1dpk_-2-0-680"]').click()
-
-driver.find_element_by_xpath("//div[contains(@data-qa, 'truck-card')][last()]")
+driver.execute_script("window.scrollTo(0, 2500)")
 
 last = driver.find_element_by_xpath("//body/div[@id='page']/div[@id='root']/main[1]/div[3]/div[1]/div[4]/div[13]/div["
                                     "1]/div[1]/div[1]/div[1]/div[1]/button[1]/div[1]")
-driver.execute_script("arguments[0].click();", last)
 
-try:
-    # wait for the alert to show up
-    WebDriverWait(driver, 3).until(EC.alert_is_present(),
-                                   'Timed out waiting for PA creation ' +
-                                   'confirmation popup to appear.')
-    # if it does
-    alert = driver.switch_to.alert()
-    alert.accept()
-    print("alert accepted")
-except TimeoutException:
-    print("no alert")
+driver.execute_script("arguments[0].click();", last)
+time.sleep(3)
+driver.switch_to.frame(driver.find_element_by_css_selector("iframe[title=\"Login popup\"]"))
+
+driver.find_element_by_css_selector(".ati-id-rui h1").get_attribute("Зарегистрируйтесь")
 
 driver.quit()
